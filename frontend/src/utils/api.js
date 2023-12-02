0b5027055b5571f2478f5e85dbb84b258d0e52fb
Dian4ik <jakie16@yandex.ru>
2023-12-02 23:15:23 +0800

    base url changed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -94,7 +94,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'http://localhost:4000',
+  baseUrl: 'http://api.projectmestorus.nomoredomainsmonster.ru',
   headers: {
   'Content-type': 'application/json',
   }
