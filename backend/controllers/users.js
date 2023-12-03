1496451ebca67d8aff815cfa6890ec3d85874229
Dian4ik <jakie16@yandex.ru>
2023-12-03 15:10:41 +0800

    15пр автотесты

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -112,7 +112,8 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id },
         NODE_ENV === 'production' ? SECRET_KEY : 'some-secret-key',
-        { expiresIn: '7d' });
+        { expiresIn: '7d' },
+      );
       res.send({ token });
     })
     .catch((err) => next(err));
